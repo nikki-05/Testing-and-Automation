e15a1c20350fb6ef4398496a5a16f15d1faada72
Nikita Tiwari <109809432+nikki-05@users.noreply.github.com>
2025-07-17 12:26:11 +0530

    Add files via upload

---
+++ Alerts.java
@@ -0,0 +1,28 @@
+package org.example;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+
+public class Alerts {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        String text="Nikita";
+        System.setProperty("WebDriver.chrome.driver", "C:\\Users\\nikita.tiwari\\Downloads\\chromedriver-win64 (1)\\chromedriver-win64");
+        WebDriver driver= new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        driver.findElement(By.id("name")).sendKeys(text);
+        driver.findElement(By.cssSelector("[id='alertbtn']")).click();
+        System.out.println(driver.switchTo().alert().getText());
+        driver.switchTo().alert().accept();
+        Thread.sleep(2000);
+        driver.findElement(By.id("confirmbtn")).click();
+        System.out.println(driver.switchTo().alert().getText());
+        driver.switchTo().alert().dismiss();
+
+    }
+    }
