d2ce7ae4c5ed337f060f469cec5e388799cdd735
Nikita Tiwari <109809432+nikki-05@users.noreply.github.com>
2025-08-05 16:16:11 +0530

    Add files via upload

---
+++ ActionExample.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class ActionExample {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("Webdriver.chrome.driver", "C:\\Users\\nikita.tiwari\\Downloads\\chromedriver-win64 (1)\\chromedriver-win64");
+        WebDriver driver= new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.amazon.com/");
+        Thread.sleep(2000);
+        Actions a = new Actions(driver);
+
+        WebElement move = driver.findElement(By.id("nav-link-accountList"));
+
+        a.moveToElement(driver.findElement(By.id("twotabsearchtextbox"))).click().keyDown(Keys.SHIFT).sendKeys("hello").build().perform();
+        a.moveToElement(move).build().perform();
+        Thread.sleep(1000);
+        driver.quit();
+
+    }
+}
